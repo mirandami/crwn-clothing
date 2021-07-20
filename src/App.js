7206dbcf941ad609133a1ba909534bd79be8d80e
Lan Mi <mirandabelle8668@gmail.com>
2021-07-20 09:44:52 -0400

    update about collection-item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
 import {Switch, Route} from 'react-router-dom';
 
-const HatsPage = () => (
-    <div>
-        <h1>HATS PAGE</h1>
-    </div>
-);
+// const HatsPage = () => (
+//     <div>
+//         <h1>HATS PAGE</h1>
+//     </div>
+// );
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
         <Switch>
             <Route exact path ='/' component={HomePage}/>
             {/*extact 表明当path刚好为local：3000的时候运行homepage，如果没有exact只要path带有local 3000就会运行homepage*/}
-            <Route path='/hats' component={HatsPage}></Route>
+            <Route path='/shop' component={ShopPage}></Route>
         </Switch>
         {/*switch的作用：当有switch就相当于有了exact，这个时候就不再需要exact，当有一个match以后其他在包含这个信息的也不可以符合*/}
     </div>
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,6 +5,8 @@ import './menu-item.styles.scss'
 
 const MenuItem = ({title,imageUrl, size, history, linkUrl, match}) => (
     <div className={`${size} menu-item`} onClick={()=>history.push(`${match.url}${linkUrl}`)}>
+        {/*用history.push可以实现dynamically control，因为可以将他用在componentdidmount这样的functional call里面，*/}
+        {/*但是link的话就不行（link不可以写在function里面，match.url是where url actually match，linkurl是想要去到的地方*/}
         <div className='background-image'  style ={{
             backgroundImage: `url(${imageUrl})`
         }}></div>
