f81d9e0bdb39fd2f4e6c11e014a9c97eb763bae4
Lan Mi <mirandabelle8668@gmail.com>
2021-08-01 14:02:12 -0400

    added cartIcon and cartDropdown components to out application, also implemented cartReducer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import {connect} from "react-redux";
 
 import './App.css';
@@ -62,7 +62,12 @@ import {setCurrentUser} from "./redux/user/user.action";
                     <Route exact path ='/' component={HomePage}/>
                     {/*extact 表明当path刚好为local：3000的时候运行homepage，如果没有exact只要path带有local 3000就会运行homepage*/}
                     <Route path='/shop' component={ShopPage}></Route>
-                    <Route path='/signin' component={SignInAndSignUpPage}></Route>
+                    <Route exact path='/signin' render={() => this.props.currentUser ? (
+                        <Redirect to= '/'/>
+                        ) : (
+                            <SignInAndSignUpPage/>
+                            )
+                    }/>
                 </Switch>
                 {/*switch的作用：当有switch就相当于有了exact，这个时候就不再需要exact，当有一个match以后其他在包含这个信息的也不可以符合*/}
             </div>
@@ -70,8 +75,14 @@ import {setCurrentUser} from "./redux/user/user.action";
     }
 }
 
+const mapStateToProps = ({user}) => ({
+    currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import {Link} from "react-router-dom";
 import {connect} from "react-redux";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 import {auth} from "../firebase/firebase.utils";
 
@@ -10,7 +12,7 @@ import './header.styles.scss'
 
 
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
     <div className='header'>
         <Link className='logo-container' to ='/'>
             <Logo className='logo'></Logo>
@@ -29,12 +31,15 @@ const Header = ({currentUser}) => (
                     <Link className='option' to='/signin'>SIGN IN</Link>
                 )
             }
+            <CartIcon/>
         </div>
+        {hidden ? null: <CartDropdown/>}
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, cart:{hidden}}) => ({
+    currentUser,
+    hidden
 });
 
 export default connect(mapStateToProps)(Header)
