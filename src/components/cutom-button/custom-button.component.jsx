f564f7b162320d4e9de40bb00e3af803b171f3a6
Lan Mi <mirandabelle8668@gmail.com>
2021-08-04 21:01:56 -0400

    updated cart utils to group items in cart reducer, updated shop data

--- src/components/cutom-button/custom-button.component.jsx
+++ src/components/cutom-button/custom-button.component.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 import './custom-button.styles.scss'
 
-const CustomButton = ({children, isGoogleSignIn, ...otherprops}) => (  //children:pass the onsubmit method to here, so for here children = submit
-    <button className= {`${isGoogleSignIn ? 'google-sign-in' : '' } custom-button `}{...otherprops}>
+const CustomButton = ({children, isGoogleSignIn, inverted, ...otherprops}) => (  //children:pass the onsubmit method to here, so for here children = submit
+    <button className= {`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : '' } custom-button `}{...otherprops}>
         {children}
     </button>
 );
